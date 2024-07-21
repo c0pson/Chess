4bec01f7d9bbe79399cb0f80dbcb6c18be39bc0f
c0pson <pc312199@student.polsl.pl>
2024-07-21 16:46:43 +0200

    colors

--- src/properties.py
+++ src/properties.py
@@ -3,7 +3,9 @@ from enum import Enum
 class COLOR(str, Enum):
     BACKGROUND = '#606676'
     TILE_1 = '#708871'
+    HIGH_TILE_1 = '#94A695'
     TILE_2 = '#BEC6A0'
+    HIGH_TILE_2 = '#CFD5B8'
     TEXT = '#FEF3E2'
     TRANSPARENT = 'transparent'
 
