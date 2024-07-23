a602e0a3311c595ad497f0978f47eeb2cebe1aed
c0pson <pc312199@student.polsl.pl>
2024-07-23 17:04:04 +0200

    menus

--- src/menus.py
+++ src/menus.py
@@ -1,7 +1,10 @@
 import customtkinter as ctk
 
+from tools import resource_path, get_from_config
+from notifications import Notification
 from properties import COLOR
 from piece import Piece
+from PIL import Image
 
 class MovesRecord(ctk.CTkFrame):
     def __init__(self, master) -> None:
@@ -39,10 +42,59 @@ class MovesRecord(ctk.CTkFrame):
         space_label = ctk.CTkLabel(self, text='\n')
         space_label.pack()
 
+    def restart(self) -> None:
+        for child in self.white_scroll_frame.winfo_children():
+            child.destroy()
+        for child in self.black_scroll_frame.winfo_children():
+            child.destroy()
+
 class Options(ctk.CTkFrame):
-    def __init__(self, master, board):
-        super().__init__(master)
+    def __init__(self, master, restart_func):
+        super().__init__(master, fg_color=COLOR.BACKGROUND)
+        self.restart_func = restart_func
+        self.setting_icon = self.load_image('settings')
+        self.replay_icon = self.load_image('replay')
+        self.setting_button()
+        self.space_label()
+        self.replay_button()
+
+    def load_image(self, option: str) -> ctk.CTkImage | None:
+        setting_icon_path = resource_path(f'assets\\menu\\{option}.png')
+        try:
+            size = int(get_from_config('size')) // 1.5
+            setting_icon = Image.open(setting_icon_path).convert('RGBA')
+            return ctk.CTkImage(light_image=setting_icon, dark_image=setting_icon, size=(size, size))
+        except (FileNotFoundError, FileExistsError) as e:
+            print(f'Couldn`t load image for due to error: {e}')
+        return None
+
+    def setting_button(self) -> None:
+        self.s_icon_label = ctk.CTkLabel(self, text='', image=self.setting_icon)
+        self.s_icon_label.pack()
+        self.s_icon_label.bind('<Button-1>', self.open_settings)
+
+    def replay_button(self) -> None:
+        self.r_icon_label = ctk.CTkLabel(self, text='', image=self.replay_icon)
+        self.r_icon_label.pack()
+        self.r_icon_label.bind('<Button-1>', self.replay)
+
+    def space_label(self) -> None:
+        space = ctk.CTkLabel(self, text='\n')
+        space.pack(padx=2, pady=2)
+
+    def open_settings(self, event) -> None:
+        print('settings')
+
+    def replay(self, event) -> None:
+        self.r_icon_label.unbind('<Button-1>')
+        self.restart_func()
+        self.r_icon_label.bind('<Button-1>', self.cooldown)
+        self.master.after(1990, lambda: self.r_icon_label.unbind('<Button-1>'))
+        self.master.after(2000, lambda: self.r_icon_label.bind('<Button-1>', self.replay))
+
+    def cooldown(self, event) -> None:
+        self.notification = Notification(self.master, 'Not so fast', 0.7, 'top')
 
 class Timer(ctk.CTkFrame):
-    def __init__(self, master, board):
+    def __init__(self, master):
         super().__init__(master)
