8696c3108ed1fbed26a6bc355a2951708d82c3fa
c0pson <pc312199@student.polsl.pl>
2024-07-24 23:21:15 +0200

    open assets folder in explorer | gui changes

--- src/menus.py
+++ src/menus.py
@@ -1,11 +1,12 @@
 import customtkinter as ctk
+import subprocess
+import platform
 import os
 
-from tools import resource_path, get_from_config, change_config, load_menu_image
+from tools import get_from_config, change_config, load_menu_image, resource_path
 from notifications import Notification
-from properties import COLOR
+from properties import COLOR, STRING
 from piece import Piece
-from PIL import Image
 
 class MovesRecord(ctk.CTkFrame):
     def __init__(self, master) -> None:
@@ -15,33 +16,37 @@ class MovesRecord(ctk.CTkFrame):
         self.moves: list[list[str]] = []
 
     def record_move(self, moved_piece: Piece, previous_coords: tuple[int, int] | None = None, capture: bool = False, castle: str | None = None, check: bool = False, checkmate: bool = False, promotion: str = '') -> None:
-        """castle: queenside | kingside"""
         y_axis: list[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         x, y = 8 - moved_piece.position[0], y_axis[moved_piece.position[1]]
         prev_x = 8 - previous_coords[0] if previous_coords else ''
         prev_y = y_axis[previous_coords[1]] if previous_coords else ''
         piece_name = moved_piece.__class__.__name__[0] if not moved_piece.__class__.__name__ == 'Pawn' else ''
         if not castle:
-            notation = f'{'+' if check and not checkmate else ''}{'#' if checkmate else ''}{'x' if capture else ''}{piece_name}{prev_y}{prev_x}-{y}{x}{promotion if promotion else ''}'
+            notation = f' {'+' if check and not checkmate else ''}{'#' if checkmate else ''}{'x' if capture else ''}{piece_name}{prev_y}{prev_x}-{y}{x}{promotion if promotion else ''}'
         else:
-            notation = f'{'+' if check and not checkmate else''}{'#' if checkmate else ''}{'0-0-0' if castle == 'queenside' else '0-0'}'
+            notation = f' {'+' if check and not checkmate else''}{'#' if checkmate else ''}{'0-0-0' if castle == 'queenside' else '0-0'}'
         current_frame = self.white_scroll_frame if moved_piece.color == 'w' else self.black_scroll_frame
         ctk.CTkLabel(current_frame, text=notation, font=self.font).pack(side=ctk.BOTTOM)
 
     def create_frames(self) -> None:
-        self.white_scroll_frame = ctk.CTkScrollableFrame(self, scrollbar_button_color=COLOR.NOTATION_BACKGROUND_W,
-                                                        fg_color=COLOR.NOTATION_BACKGROUND_W,
-                                                        scrollbar_button_hover_color=COLOR.NOTATION_BACKGROUND_W)
-        white_label = ctk.CTkLabel(self, text='White', font=self.font, text_color=COLOR.TEXT)
-        self.black_scroll_frame = ctk.CTkScrollableFrame(self, scrollbar_button_color=COLOR.NOTATION_BACKGROUND_B,
-                                                        fg_color=COLOR.NOTATION_BACKGROUND_B,
-                                                        scrollbar_button_hover_color=COLOR.NOTATION_BACKGROUND_B)
         black_label = ctk.CTkLabel(self, text='Black', font=self.font, text_color=COLOR.DARK_TEXT)
-        self.black_label = ctk.CTkLabel(self.white_scroll_frame, text='Black', font=self.font)
         black_label.pack(side=ctk.TOP, padx=1, pady=1)
-        self.black_scroll_frame.pack(side=ctk.TOP, padx=2, pady=2, fill=ctk.Y, expand=True)
-        white_label.pack(side=ctk.TOP, padx=1, pady=1)
-        self.white_scroll_frame.pack(side=ctk.TOP, padx=2, pady=2, fill=ctk.Y, expand=True)
+        additional_frame = ctk.CTkFrame(self, fg_color=COLOR.TRANSPARENT, corner_radius=0,
+                                        border_color=COLOR.DARK_TEXT, border_width=4)
+        additional_frame.pack(side=ctk.TOP, padx=15, expand=True, fill=ctk.Y)
+        self.black_scroll_frame = ctk.CTkScrollableFrame(additional_frame, scrollbar_button_color=COLOR.NOTATION_BACKGROUND_B,
+                                                        fg_color=COLOR.NOTATION_BACKGROUND_B, corner_radius=0,
+                                                        scrollbar_button_hover_color=COLOR.NOTATION_BACKGROUND_B,)
+        self.black_scroll_frame.pack(side=ctk.TOP, padx=3, pady=4, fill=ctk.Y, expand=True)
+        white_label = ctk.CTkLabel(self, text='White', font=self.font, text_color=COLOR.TEXT)
+        white_label.pack(side=ctk.TOP, padx=0, pady=0)
+        additional_frame = ctk.CTkFrame(self, fg_color=COLOR.TRANSPARENT, corner_radius=0,
+                                        border_color=COLOR.DARK_TEXT, border_width=4)
+        additional_frame.pack(side=ctk.TOP, padx=15, expand=True, fill=ctk.Y)
+        self.white_scroll_frame = ctk.CTkScrollableFrame(additional_frame, scrollbar_button_color=COLOR.NOTATION_BACKGROUND_W,
+                                                        fg_color=COLOR.NOTATION_BACKGROUND_W, corner_radius=0,
+                                                        scrollbar_button_hover_color=COLOR.NOTATION_BACKGROUND_W)
+        self.white_scroll_frame.pack(side=ctk.TOP, padx=3, pady=4, fill=ctk.Y, expand=True)
         space_label = ctk.CTkLabel(self, text='\n')
         space_label.pack()
 
@@ -64,12 +69,12 @@ class Options(ctk.CTkFrame):
 
     def setting_button(self) -> None:
         self.s_icon_label = ctk.CTkLabel(self, text='', image=self.setting_icon)
-        self.s_icon_label.pack()
+        self.s_icon_label.pack(side=ctk.TOP, padx=10, pady=5)
         self.s_icon_label.bind('<Button-1>', self.open_settings)
 
     def replay_button(self) -> None:
         self.r_icon_label = ctk.CTkLabel(self, text='', image=self.replay_icon)
-        self.r_icon_label.pack()
+        self.r_icon_label.pack(side=ctk.TOP, padx=10, pady=0)
         self.r_icon_label.bind('<Button-1>', self.replay)
 
     def space_label(self) -> None:
@@ -89,17 +94,14 @@ class Options(ctk.CTkFrame):
     def cooldown(self, event) -> None:
         self.notification = Notification(self.master, 'Not so fast', 0.7, 'top')
 
-class Timer(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
 class Settings(ctk.CTkFrame):
     def __init__(self, master, restart_func, update_assets_func) -> None:
-        super().__init__(master, fg_color=COLOR.BACKGROUND)
+        super().__init__(master, fg_color=COLOR.BACKGROUND, corner_radius=0)
         self.place(relx=0, rely=0, relwidth=1, relheight=1)
         self.close_image = load_menu_image('close')
         self.close_button()
         self.choose_theme()
+        self.open_assets_folder()
         self.previous_theme: None | str = None
         self.choice: None | str = None
         self.restart_func = restart_func
@@ -109,37 +111,48 @@ class Settings(ctk.CTkFrame):
     def list_directories_os(path) -> list:
         try:
             entries = os.listdir(path)
-            directories = [entry for entry in entries if os.path.isdir(os.path.join(path, entry))]
+            directories = [
+                entry for entry in entries
+                if os.path.isdir(os.path.join(path, entry)) and os.listdir(os.path.join(path, entry))
+            ]
             return directories
         except FileNotFoundError:
             return []
 
     def close_button(self) -> None:
-        close_button = ctk.CTkLabel(self, text='', font=ctk.CTkFont('Tiny5',24),
-                                    image=self.close_image)
+        top_frame = ctk.CTkFrame(self, fg_color=COLOR.TRANSPARENT)
+        top_frame.pack(side=ctk.TOP, padx=0, pady=0, fill=ctk.X)
+        settings_text = ctk.CTkLabel(top_frame, text='Settings', font=ctk.CTkFont('Tiny5', 38),
+                                    text_color=COLOR.DARK_TEXT, anchor=ctk.N)
+        settings_text.pack(side=ctk.LEFT, padx=20, anchor=ctk.NW)
+        close_button = ctk.CTkLabel(top_frame, text='', font=ctk.CTkFont('Tiny5', 24),
+                                    image=self.close_image, anchor=ctk.S)
         close_button.bind('<Button-1>', self.on_close)
-        close_button.pack(side=ctk.TOP, anchor=ctk.NE, padx=10, pady=10)
+        close_button.pack(side=ctk.RIGHT, anchor=ctk.NE, padx=10, pady=10)
 
     def create_theme_button(self, frame, theme: str) -> None:
         theme_button = ctk.CTkButton(frame, text=theme, command=lambda: self.select_theme(theme),
-                                        font=ctk.CTkFont('Tiny5', 38))
-        theme_button.pack(side=ctk.LEFT, padx=5, pady=5)
+                                        font=ctk.CTkFont('Tiny5', 30), corner_radius=0,
+                                        fg_color=COLOR.TILE_1, hover_color=COLOR.HIGH_TILE_1)
+        theme_button.pack(side=ctk.LEFT, padx=4, pady=4, expand=True)
 
     def choose_theme(self) -> None:
         self.previous_theme = str(get_from_config('theme'))
         themes = self.list_directories_os('assets')
         if not themes:
             return
+        text = ctk.CTkLabel(self, text='Themes: ', font=ctk.CTkFont('Tiny5', 32), text_color=COLOR.TEXT)
+        text.pack(side=ctk.TOP, anchor=ctk.SW, padx=75, pady=0)
         themes.remove('menu') if 'menu' in themes else themes
-        frame = ctk.CTkFrame(self)
-        frame.pack(side=ctk.TOP, padx=15, pady=5, anchor=ctk.W)
-        text = ctk.CTkLabel(frame, text='Themes: ', font=ctk.CTkFont('Tiny5', 38))
-        text.pack(side=ctk.TOP, anchor=ctk.SW, padx=5, pady=5)
-        for i, theme in enumerate(themes, 1):
-            if not i % 7:
-                frame = ctk.CTkFrame(self)
-                frame.pack(side=ctk.TOP, padx=15, pady=5, anchor=ctk.W)
+        frame = ctk.CTkScrollableFrame(self, fg_color=COLOR.TILE_2, scrollbar_button_color=COLOR.DARK_TEXT,
+                                        scrollbar_button_hover_color=COLOR.DARK_TEXT, orientation=ctk.HORIZONTAL,
+                                        height=70, corner_radius=0, scrollbar_fg_color=COLOR.DARK_TEXT)
+        frame.pack(side=ctk.TOP, padx=80, pady=5, anchor=ctk.W, fill=ctk.X)
+        for theme in themes:
             self.create_theme_button(frame, theme)
+        warning_text = ctk.CTkLabel(self, text=STRING.ASSETS_WARNING, font=ctk.CTkFont('Tiny5', 18),
+                                    text_color=COLOR.CLOSE)
+        warning_text.pack(side=ctk.TOP, anchor=ctk.SW, padx=100, pady=0)
 
     def select_theme(self, choice: str) -> None:
         self.choice = choice
@@ -153,3 +166,25 @@ class Settings(ctk.CTkFrame):
             self.restart_func
         self.update_assets_func()
         self.destroy()
+
+    @staticmethod
+    def open_file_explorer(path: str):
+        system = platform.system()
+        if system == 'Windows':
+            os.startfile(path)
+        elif system == 'Darwin':
+            subprocess.run(['open', path])
+        elif system == 'Linux':
+            subprocess.run(['xdg-open', path])
+
+    def open_assets_folder(self) -> None:
+        additional_frame = ctk.CTkFrame(self, fg_color=COLOR.TILE_2, corner_radius=0)
+        additional_frame.pack(side=ctk.TOP, padx=80, pady=0, fill=ctk.X)
+        text_label = ctk.CTkLabel(additional_frame, text='Open assets folder', text_color=COLOR.TEXT,
+                                    font=ctk.CTkFont('Tiny5', 32))
+        text_label.pack(side=ctk.LEFT, padx=10, pady=4, anchor=ctk.N)
+        open_button = ctk.CTkButton(additional_frame, text='OPEN', font=ctk.CTkFont('Tiny5', 20),
+                                    text_color=COLOR.TEXT, command=lambda: self.open_file_explorer('assets'),
+                                    fg_color=COLOR.TILE_1, hover_color=COLOR.HIGH_TILE_2,
+                                    corner_radius=0)
+        open_button.pack(side=ctk.LEFT, padx=10, pady=4, anchor=ctk.S)
