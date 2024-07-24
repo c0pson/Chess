617559681a8211921ec7251c97e337aee7ee7540
c0pson <pc312199@student.polsl.pl>
2024-07-24 23:20:34 +0200

    icon | changed priority

--- src/main.py
+++ src/main.py
@@ -13,25 +13,31 @@ class MainWindow(ctk.CTk):
         self.title('Chess')
         self.geometry(self.set_window_size())
         size = int(get_from_config('size'))
-        self.minsize(((size + 2) * 9 + 40)+ 400, (size * 9 + 40))
+        self.minsize(((size + 2) * 10 + 40)+ 400, ((size + 2) * 10 + 40))
         self.load_font()
         self.moves_record = MovesRecord(self)
         self.moves_record.pack(side=ctk.RIGHT, padx=10, pady=10, fill=ctk.Y)
-        self.board = Board(self, self.moves_record, size)
-        self.board.pack(side=ctk.RIGHT, padx=10, pady=10, expand=True)
         self.options = Options(self, self.restart_game, self.update_assets)
-        self.options.pack(side=ctk.RIGHT, padx=10, pady=10, fill=ctk.Y)
+        self.options.pack(side=ctk.LEFT, padx=10, pady=10, fill=ctk.Y)
+        self.board = Board(self, self.moves_record, size)
+        self.board.pack(side=ctk.RIGHT, padx=10, pady=10, expand=True, ipadx=5, ipady=5)
         self.theme: str = str(get_from_config('theme'))
+        self.set_icon()
 
     def load_font(self) -> None:
+        font = get_from_config('font_file_name')
         if os.name == 'nt':
-            ctk.FontManager.windows_load_font(resource_path('fonts\\Tiny5-Regular.ttf'))
+            ctk.FontManager.windows_load_font(resource_path(f'fonts\\{font}'))
         else:
-            ctk.FontManager.load_font(resource_path('fonts/Tiny5-Regular.ttf'))
+            ctk.FontManager.load_font(resource_path(f'fonts/{font}'))
+
+    def set_icon(self) -> None:
+        if os.name == 'nt':
+            self.iconbitmap(resource_path('assets\\logo.ico'))
 
     def set_window_size(self) -> str:
         size: int = int(get_from_config('size'))
-        size = (size+2) * 9 + 40
+        size = (size+2) * 10 + 40
         center_pos: str = f'+{(self.winfo_screenwidth()-size-300)//2}+75'
         return f'{size + 300}x{size}{center_pos}'
 
@@ -43,7 +49,7 @@ class MainWindow(ctk.CTk):
         for row in self.board.board:
             for cell in row:
                 if cell.figure:
-                    cell.figure.update_image() 
+                    cell.figure.update_image()
 
 if __name__ == "__main__":
     ctk.deactivate_automatic_dpi_awareness()
